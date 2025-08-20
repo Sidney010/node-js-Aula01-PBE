3baea1b8f6e772c1d74b7b7ce26a1799ea43c6d2
SIDNEY CAMPOS ARAGÃO <25132876@CFP127ALUNO.LOCAL>
2025-08-20 16:45:37 -0300

    Adicionado

--- AULA 04/app.js
+++ AULA 04/app.js
@@ -4,54 +4,42 @@
  * Data: 18/08/2025
  * Versão: 1.0
  ************************************************************/
+ 
+//Gera um número aleatório
+const numeroAleatorio = Math.floor(Math.random() * 101)
+let i = numeroAleatorio
 
+//Importa da biblioteca do readLine para manipular entrada de dados no terminal
+const readLine = require('readline')
 
+//Criando um objeto de entrada e saída de dados no terminal
+const entradaDeDados = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
+let condicao = true // Esta variável não é mais necessária para o loop, mas a mantive para mostrar a lógica
 
+console.log('--- Jogo de Adivinhação ---');
+console.log('Tente adivinhar o número que eu pensei (entre 1 e 100)!')
+let contadorVezes = 1
+function fazerPergunta() {
+    entradaDeDados.question('Digite um número: ', (numeroDigitado) => {
+        const numero = Number(numeroDigitado)
+        if (numero === numeroAleatorio) {
+            console.log(`Você VENCEU! O número premiado era ${numeroAleatorio}`)
+            entradaDeDados.close()
+            console.log(`Você utilizou ${contadorVezes} vezes para vencer`)
+        } else {
+            if (numero > numeroAleatorio) {
+                console.log('O número premiado é MENOR. Tente de novo.')
+            } else {
+                console.log('O número premiado é MAIOR. Tente de novo.')
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-// //Gera um número aleatório
-// const numeroAleatorio = Math.floor(Math.random() * 101)
-// i = numeroAleatorio
-
-// //Importa da biblioteca do readLine para manipular entrada de dados no terminal
-// const readLine = require('readline')
-
-// //Criando um objeto de entrada e saída de dados no terminal
-// const entradaDeDados = readLine.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// })
-
-// let condicao = true
-// do {
-//     entradaDeDados.question('Digite um número: ', function (numeroDigitado) {
-//         numero = Number(numeroDigitado)
-
-//         if (numero === i) {
-//             condicao = false
-//             console.log(`Você venceu! O número premiado é ${i}`)
-//             entradaDeDados.close() // encerra o readline
-//         } else if (numero > numeroAleatorio) {
-//             console.log('O número premiado é menor')
-//         } else if (numero < numeroAleatorio) {
-//             console.log(' O número premiado é maior')
-//         }
-
-//         // Se ainda não acertou, o laço continua
-//         if (condicao) {
-//             // Chama de novo a pergunta (simula o loop)
-//             return
-//         }
-//     })
-// } while (condicao)
+            contadorVezes++
+            fazerPergunta()
+        }
+    });
+}
+fazerPergunta()
